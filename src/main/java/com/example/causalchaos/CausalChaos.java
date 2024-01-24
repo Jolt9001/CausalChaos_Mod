be26acece81708c847fa7168f4f976811cccfcf1
Jolt9001 <71778923+Jolt9001@users.noreply.github.com>
2024-01-23 19:37:08 -0500

    Merge origin/master
    Conflicts:
    	.gitignore

--- src/main/java/com/example/causalchaos/CausalChaos.java
+++ src/main/java/com/example/causalchaos/CausalChaos.java
@@ -1,6 +1,8 @@
 package com.example.causalchaos;
 
 import com.mojang.logging.LogUtils;
+import java.io.ObjectInputFilter.Config;
+import java.lang.System.Logger;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.food.FoodProperties;
