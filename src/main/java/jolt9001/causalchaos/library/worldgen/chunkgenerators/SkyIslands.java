56c8bea6578863fd0436ee07b21497cefbbad5fe
Jolt9001 <71778923+Jolt9001@users.noreply.github.com>
2025-07-29 14:36:11 -0400

    Brought back accidentally deleted files oops

---
+++ src/main/java/jolt9001/causalchaos/library/worldgen/chunkgenerators/SkyIslands.java
@@ -0,0 +1,11 @@
+package jolt9001.causalchaos.library.worldgen.chunkgenerators;
+
+public class SkyIslands {
+
+    /**
+     * similar to the Aether mod
+     * use similar worldgen but the same biomes as the overworld (use the same biome seed as the overworld does)
+     * new block textures and resources exclusive to this dimension
+     * force some vanilla structures to form (villages, mineshafts, etc) using the available materials
+     */
+}
